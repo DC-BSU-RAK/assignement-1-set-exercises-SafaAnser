8490a0da652b1e20d0c87d2eec0056d079cf05b6
SafaAnser <safaanser.rak@gmail.com>
2025-04-16 06:02:27 +0400

    chapter 6 - Petrol calculator

---
+++ Chapter-6-Javascript/Petrol calculator (Mandatory)/script.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    const calculateButton = document.querySelector("#butn button");
+    const resetButton = document.querySelector("button[type='reset']");
+    
+    calculateButton.addEventListener('click', calculate);
+    resetButton.addEventListener('click', reset);
+}
+
+function calculate() {
+    const petrol_cost = parseFloat(document.querySelector('#petrol_cost').value);
+    const liters = parseFloat(document.querySelector('#liters').value);
+
+    if (isNaN(petrol_cost) || isNaN(liters)) {
+        document.querySelector('#totalAmount').innerText = "Please choose the numbers";
+        return;
+    }
+
+    const totalAmount = petrol_cost * liters;
+    document.querySelector('#totalAmount').innerText = totalAmount.toFixed(2);
+}
+
+function reset() {
+    document.querySelector('#petrol_cost').value = '';
+    document.querySelector('#liters').value = '';
+    document.querySelector('#totalAmount').innerText = '';
+}
